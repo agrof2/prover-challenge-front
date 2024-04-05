565e0b2dd01c76cef3637ada44c9e7b8dbfce431
Felipe Augusto <salvadoraugusto@hotmail.com>
2024-04-05 09:11:16 -0300

    ajustando texto

--- src/App.js
+++ src/App.js
@@ -11,16 +11,15 @@ const ConsultPhraseComponent = () => {
       console.log(phrase)
       fetch(`http://localhost:8080/processor?phrase=${phrase}`)
           .then(response => response.json())
-          // .then(data => setResult(JSON.stringify(data)))
           .then(data => setResult(`Frase: ${data.phrase}\nQuantidade de palavras unicas: ${data.uniqueWordQuantity}\nOcorrencias: ${data.occurrencesPerWord.map(e => `Palavra: ${e.word} Ocorrencias: ${e.occurences}`).join('\n')}`))
           .catch(error => console.error('Error:', error));
   };
 
   return (
       <div>
-        <input type="text" value={phrase} onChange={handleChange} placeholder="Insert your phrase here" />
-        <button onClick={handleSubmit}>Consult Phrase</button>
-        <p>Result: {result}</p>
+        <input type="text" value={phrase} onChange={handleChange} placeholder="Insira sua frase para processamento: " />
+        <button onClick={handleSubmit}>Processar</button>
+        <p>{result}</p>
       </div>
   );
 };
